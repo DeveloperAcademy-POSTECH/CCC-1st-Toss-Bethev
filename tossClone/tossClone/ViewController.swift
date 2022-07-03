c0e1c0c309d54f78f3e8bf0f7946205638bec261
Bethev <jmail88@naver.com>
2022-07-03 19:59:53 +0900

    프로젝트 생성

---
+++ tossClone/tossClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tossClone
+//
+//  Created by leejunmo on 2022/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
